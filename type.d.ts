39f1e6368d5248908cfed324403663ab8966099e
silence_zhpf <silence_zhpf@aliyun.com>
2023-01-04 11:44:10 +0800

    chore: ✨ 为Config添加更多的请求参数

--- type.d.ts
+++ type.d.ts
@@ -2,13 +2,26 @@
  * Copyright (c) 2022.  by silence_zhpf
  */
 declare type Store = Record<string, any>;
-declare type Method = "POST" | "GET";
 declare type IRequest = Omit<RequestInit, "method">;
+declare type RequestMode = "core" | "no-core" | "same-origin";
+declare type RequestMethod = "GET" | "POST";
 declare type Config = {
   timeout?: number;
   requestInterceptors?: ((requestInit: IRequest) => IRequest)[];
   responseInterceptors?: any[];
   requestType?: "json" | "form";
+  /**
+   * 请求方法
+   */
+  method?: RequestMethod;
+  /**
+   * 请求头
+   */
+  headers?: any;
+  /**
+   * 请求模式
+   */
+  mode?: RequestMode;
 };
 type PostParams = {
   data: any;
